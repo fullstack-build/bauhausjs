f07eeaf303f5b7c3470644368c91a91101dc23a6
Eugene Terehov <opensource@terehov.de>
2016-04-09 19:26:19 +0800

    boot: autoupdate user table

---
+++ server/boot/autoupdate.js
@@ -0,0 +1,19 @@
+/// <reference path="../../typings/node.d.ts" />
+'use strict';
+function autoUpdate(app) {
+    var ds = app.dataSources.postgres;
+    var appModels = ['user'];
+    ds.isActual(appModels, function (err, actual) {
+        if (!actual) {
+            ds.autoupdate(appModels, function (err) {
+                if (err) {
+                    throw (err);
+                }
+            });
+        }
+    });
+    console.log("Migrated: " + appModels.join());
+}
+;
+module.exports = autoUpdate;
+//# sourceMappingURL=autoupdate.js.map
